410c9f2127f380e732c9b13d653799d0b728f492
Jose Zapata <joferzz1989@gmail.com>
2019-10-20 22:07:06 -0400

    iteraciones

--- src/fchips/archivos.java
+++ src/fchips/archivos.java
@@ -12,10 +12,10 @@ public class archivos {
        
     public archivos() {
         try {
-            Scanner scanner = new Scanner(new File("test.txt"));
+            Scanner scanner = new Scanner(new File("input.txt"));
             while (scanner.hasNextLine()) {
                 String p_linea;
-                p_linea = scanner.nextLine();                
+                p_linea = scanner.nextLine();
                 this.proceso(Integer.parseInt(p_linea.split(" ")[0]), Integer.parseInt(p_linea.split(" ")[1]), scanner.nextLine().toCharArray());               
             }
             scanner.close();
@@ -37,14 +37,45 @@ public class archivos {
         }
         
         for(int i = 1; i <= iteraciones; i++) {
-            System.out.println("ships " + ships.toString());
             ships = this.iteracion(ships);
             System.out.println("iteracion " + i);
-            System.out.println(ships.toString());
+            System.out.println(ships);
         }
+        
     }
     
     public char[] iteracion(char[] ships) {
+        char[] aux_ships = new char[ships.length];
         
+        for (int k = 0; k < ships.length; k++) {
+            String auxiliar = "";
+            if (k == 0) {
+                auxiliar = ships[ships.length-1] + "" + ships[k] + "" + ships[k+1];
+            } else if (k == (ships.length - 1)) {
+                auxiliar = ships[k-1] + "" + ships[k] + "" + ships[0];
+            } else {
+                auxiliar = ships[k-1] + "" + ships[k] + "" + ships[k+1];
+            }
+            
+            aux_ships[k] = this.color_ship(auxiliar);
+        }
+        
+        return aux_ships;
+    }
+    
+    public char color_ship(String cadena) {
+        int count_w = 0;
+        int count_b = 0;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == 'W') {
+                count_w++;
+            }
+            else {
+                count_b++;
+            }
+        }
+        
+        return (count_w > count_b) ? "W".charAt(0) : "B".charAt(0);
     }
 }
